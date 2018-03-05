239ee325d53c709a509ec227c834ea8a7114bb4a
Victordroid <36991460+Victordroid@users.noreply.github.com>
2018-03-05 10:42:11 +0100

    Initial commit

---
+++ app/src/main/java/colecciones/com/example/primerasvistas/MainActivity.java
@@ -0,0 +1,13 @@
+package colecciones.com.example.primerasvistas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
